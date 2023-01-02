edc9771c3d684aa2cc75cb8b9bb4dbcec2aec6b7
rakshithpt <rakshithpt.1si17te026@gmail.com>
2023-01-02 21:58:14 +0530

    new file push

---
+++ jsp-wfm/src/main/java/com/jsp/jspwfm/Controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.jsp.jspwfm.Controllers;
+
+import com.jsp.jspwfm.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @RequestMapping("/getUser")
+    public ResponseEntity testFetchUser(@RequestParam String username,@RequestParam String password)
+    {
+        return new ResponseEntity<>(userService.getUser(username,password), HttpStatusCode.valueOf(200));
+    }
+}
